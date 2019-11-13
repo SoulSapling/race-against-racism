a287384bb33d9a37f16703c452e3c87e106edad1
101784431 <101784431@student.swin.edu.au>
2019-11-13 19:48:35 +1100

    +New test scene +Parented character, background and hurdles to the game world +New script for the character to control states and use colliders

---
+++ RFH Quiz 2019/Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour {
+	
+	private bool isRunning = false;
+	public bool hasStarted = true;
+	public float speed = 10;
+	private Animator animator;
+	
+	private void Start()
+	{
+		animator = gameObject.GetComponent<Animator>();
+	}
+	
+	private void Update()
+	{
+		if(hasStarted == true)
+		{
+			animator.SetBool("isRunning", isRunning);
+			if(isRunning == true)
+			{
+				transform.Translate(Vector2.right * speed * Time.deltaTime);
+			}
+		}
+	}
+	
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.tag == "Hurdle")
+		{
+			isRunning = false;
+			animator.SetBool("jump", true);
+		}
+		print("get ready to jump");
+	}
+}
